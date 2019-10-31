17c138cd2aeb1bc2391785a43df0439b8331789e
Nicholas <nickdarash@gmail.com>
2019-10-30 21:57:03 -0700

    Added like buttons

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -26,7 +26,7 @@ class Post extends React.Component {
       
     })
     .then(res => {
-      this.setState({posts: this.state.postInfo.posts.push(res.data.post)});
+      this.setState({posts: this.state.postInfo.posts.unshift(res.data.post)});
     })
     .catch((err) => {
       if(err) throw err;
@@ -41,7 +41,6 @@ class Post extends React.Component {
     });
   }
 
-  
   componentDidUpdate = () => {
     if(!this.state.postInfo.posts.length && this.state.postInfo.postsAvailable){
       this.fetchPosts(); 
@@ -54,6 +53,50 @@ class Post extends React.Component {
     }
   }
 
+  postDiff = (date) => {
+    const currentDate = new Date();
+    const postDate = new Date(date);
+    const minutes = (currentDate.getTime() - postDate.getTime())/60000;
+    const seconds = (currentDate.getTime() - postDate.getTime())/1000;
+    if(minutes>525600){
+      return `${Math.floor(minutes/525600)} years ago`;
+    }else if(minutes>43200){
+      return `${Math.floor(minutes/43200)} months ago`;
+    }else if(minutes>1440){
+      return `${Math.floor(minutes/1440)} days ago`;
+    }else if(minutes>60){
+      return `${Math.floor(minutes/60)} hours ago`;
+    }else if(minutes){
+      return `${Math.floor(minutes)} minutes ago`;
+    }else if(seconds>30){
+      return `${Math.floor(seconds)} seconds ago`;
+    }else{
+      return `just now`;
+    }
+  }
+
+  like = (id) => {
+    axios.put(`${this.props.general.domain}:${this.props.general.port}/api/private/posts/like`,
+      {
+        id: id
+      },
+      {
+        headers: {
+          jwt: this.props.general.jwt
+        }
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        if(err) throw err;
+      }
+    );
+  }
+
+  dislike = (id) => {
+    console.log(id);
+  }
 
   render(){
     return(
@@ -62,7 +105,11 @@ class Post extends React.Component {
         {this.state.postInfo.posts.map((post) => {
           return(
             <div className="post" key={post._id}>
-              <p>{post.text}</p>
+              <p className="diff">{this.postDiff(post.timeStamp)}</p>
+              <p className="text">{post.text}</p>
+              <button className="like" onClick={() => this.like(post._id)} >+</button>
+              <button className="dislike" onClick={() => this.dislike(post._id)} >-</button>
+              <p className="likes">{post.likes}</p>
             </div>
           );
         })
